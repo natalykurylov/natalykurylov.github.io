ac381de6b7ba314dd959ec51c0a6af8647df9472
Nataly <94167247+natalykurylov@users.noreply.github.com>
2022-03-02 17:42:41 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -4,6 +4,28 @@ function makeitBigger()
     document.getElementById("text").style.fontSize = "24pt";
 }
 
+  function fancy()
+{
+    document.getElementById("betty").checked  = false;  
+    document.getElementById("textfield").style.fontWeight = "bold";
+    document.getElementById("textfield").style.color = "0000FF";
+    document.getElementById("textfield").style.textDecoration = "underline";
+    document.getElementById("textfield").style.textTransform = "uppercase";
+    document.getElementById("textfield").style.fontStyle = "Arial";
+}
+
+function betty()
+{
+    document.getElementById("fancy").checked  = false; 
+	document.getElementById("textfield").style.fontWeight = "normal";
+	document.getElementById("textfield").style.color = "#000000";
+	document.getElementById("textfield").style.textDecoration = "none";
+	document.getElementById("textfield").style.fontSize = "initial";
+	document.getElementById("textfield").style.textTransform = "initial";
+    document.getElementById("textfield").style.fontSize = "12pt";
+    document.getElementById("textfield").style.fontStyle = "Arial";
+}
+
 function letsMoo() {  
     let str = document.getElementById("text");
     str.style.textTransform = "uppercase";
