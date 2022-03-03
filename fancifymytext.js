6ea1ceecfb3af16cb3676a331b8311a8b7b626cc
Nataly <94167247+natalykurylov@users.noreply.github.com>
2022-03-02 17:31:42 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -3,3 +3,10 @@ function makeitBigger()
     alert("Hello World");
     document.getElementById("text").style.fontSize = "24pt";
 }
+
+function letsMoo() {  
+    let str = document.getElementById("text");
+    str.style.textTransform = "uppercase";
+    let parts = str.value.split(".");
+    str.value = parts.join("-Moo.");
+}
